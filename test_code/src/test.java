02c4ad03e3b74ae63f6055c5374ffa38f57e6870
fff <fff@seu.edu.cn>
2026-01-07 17:11:47 +0800

    修改了编译工具的版本

---
+++ test_code/src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args){
+        String a=new String("abc");
+        String b=new String("abc");
+        System.out.println(a.equals(b));
+    }
+}
