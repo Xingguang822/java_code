a1492005c4c73acdc765d5254daeb18cbabb9c80
fff <fff@seu.edu.cn>
2026-02-18 05:24:11 +0800

    修改了测试代码并移除了编译原理实验的代码，但输出结果依然保留

--- test_code/src/test.java
+++ test_code/src/test.java
@@ -1,7 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class test {
-    public static void main(String[] args){
-        String a=new String("abc");
-        String b=new String("abc");
-        System.out.println(a.equals(b));
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("test_code//a.txt");
+        String str="keguanlaoyezuishuai\r\n666";
+        byte[] bytes=str.getBytes();
+        fos.write(bytes);
+        fos.close();
     }
 }
